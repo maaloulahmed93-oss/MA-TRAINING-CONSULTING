306260d94ed6293c97dc79b2f229eabaa96e8ea3
maaloulahmed93-oss <maaloulahmed93@gmail.com>
2025-09-12 13:14:03 +0200

    chore: update code for main site and admin panel

--- admin-panel/vite.config.ts
+++ admin-panel/vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/admin/',
+  base: '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   server: {
     port: 8536
   }
