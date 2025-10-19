86e9a53bd60847d73fe66076d72f5b459ba19f90
maaloulahmed93-oss <maaloulahmed93@gmail.com>
2025-10-20 00:31:48 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -101,7 +101,7 @@ app.use((req, res, next) => {
   }
   
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
   res.header('Access-Control-Allow-Credentials', 'true');
   
   // Handle preflight requests
